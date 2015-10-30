3e74eb6830255e947d1ca7ea1c94fb33002363df
Volodymyr Halamiy <halamiy@gmail.com>
2015-10-30 16:51:44 +0200

    Adding Facebook Login.

--- SocialNetwork/ViewController.m
+++ SocialNetwork/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
 
 @interface ViewController ()
 
@@ -16,6 +18,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    FBSDKLoginButton *loginButton = [[FBSDKLoginButton alloc] init];
+    loginButton.center = self.view.center;
+    [self.view addSubview:loginButton];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
